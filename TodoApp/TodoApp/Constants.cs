72fb85b86cd9075b28c1eac516c2ee217f498c50
lahiaouni <lahiaouni.abd@gmail.com>
2017-12-09 22:54:57 +0000

    Changed RestUrl

--- TodoApp/TodoApp/Constants.cs
+++ TodoApp/TodoApp/Constants.cs
@@ -4,7 +4,7 @@
     public static class Constants
     {
         // URL of REST service
-        public static string RestUrl = "http://todoapi20171209102122.azurewebsites.net";
+        public static string RestUrl = "http://todoapi20171209102122.azurewebsites.net/api/ToDoItems";
         // Credentials that are hard coded into the REST service
         public static string Username = "abd";
         public static string Password = "Haycha_4";
